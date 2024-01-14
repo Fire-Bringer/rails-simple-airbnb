df5024478dd1e062594468a8ef57536bc0834bb9
Rashad DuPaty <radupaty@gmail.com>
2024-01-14 14:23:28 +0900

    set presence and uniqueness validations

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,7 @@
+# frozen-string-literal: true
+
+# Flat Model
 class Flat < ApplicationRecord
+  validates :name, :address, :description, :price_per_night, :number_of_guests, presence: true
+  validates :address, uniqueness: true
 end
