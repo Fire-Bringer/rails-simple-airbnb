de143f502cb49187d4a86d2fdaab670b37226f90
Rashad DuPaty <radupaty@gmail.com>
2024-01-14 14:50:11 +0900

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,13 @@ Flat.destroy_all
 puts 'Creating 10 new flats'
 10.times do
   flat = Flat.create!(
-    name: Faker::Restaurant.name,
+    name: Faker::Lorem.sentence,
     address: Faker::Address.full_address,
-    description: Faker::PhoneNumber.phone_number,
-    price_per_night: %w[chinese italian japanese french belgian].sample,
-    number_of_guests: 
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    price_per_night: Faker::Number.number(digits: 3),
+    number_of_guests: Faker::Number.number(digits: 1)
   )
-  puts "#{Flat.name} has been created"
+  puts "#{flat.name} has been created"
 end
 
 puts 'Done!'
